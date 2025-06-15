45c06a0e83fa4054fe561a941ac751857a110c1a
ANTONIO DUARTE MARCOS JUNIOR <antonio.junior@indicium.tech>
2025-06-15 00:55:24 +0000

    finalizada dimensao produtos

--- models/staging/erp/stg_erp__suppliers.sql
+++ models/staging/erp/stg_erp__suppliers.sql
@@ -4,15 +4,15 @@ with
     )
     , renamed as (
         select
-            cast (id as int) as suppliers_pk
-            , cast (companyname as varchar) as suppliers_name
+            cast (id as int) as supplier_pk
+            , cast (companyname as varchar) as supplier_name
             -- , cast (contactname as varchar) as suppliers_contactname
             -- , cast (contacttitle as varchar) as suppliers_contacttitle
             -- , cast (address as varchar) as suppliers_adress
-            , cast (city as varchar) as suppliers_city
-            , cast (region as varchar) as suppliers_region
+            , cast (city as varchar) as supplier_city
+            , cast (region as varchar) as supplier_region
             -- , cast (postalcode as varchar) as suppliers_
-            , cast (country as varchar) as suppliers_country
+            , cast (country as varchar) as supplier_country
             -- , cast (phone as varchar) as suppliers_
             -- , cast (fax as varchar) as suppliers_
             -- , cast (homepag as varchar) as suppliers_
